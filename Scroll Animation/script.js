c96bb86b675627696b2ace469375133401a25ea4
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-01-02 23:09:50 +0400

    finshed

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -2,27 +2,25 @@
 
 const animationItems = document.querySelectorAll(".animationItem");
 
-const scrollAnimation =()=>{
-    animationItems.forEach((animationItems)=>{
-        const triggerPoint = window.innerHeight*0.8;
-        const {top:distanceFromTop} = animationItems.getBoundingClientRect();
+const scrollAnimation = () => {
+    animationItems.forEach((animationItem) => {
+        const triggerPoint = window.innerHeight * 0.8;
+        const { top: distanceFromTop } = animationItem.getBoundingClientRect();
 
-        animationItems.style.transition = "all 200ms linear";
+        animationItem.style.transition = "all 200ms linear";
 
-        if(distanceFromTop<triggerPoint){
-            animationItems.style.opacity = 0;
-            animationItems.style.transform = "translateY(50px)";
-            animationItems.classList.remove("opacity-0")
-        }else{
-            animationItems.style.opacity = 0;
-            animationItems.style.transform = "translateY(50px)";
-            animationItems.classList.remove("opacity-0")
-
+        if (distanceFromTop < triggerPoint) {
+            animationItem.style.opacity = 1;
+            animationItem.style.transform = "translateY(0)";
+            animationItem.classList.remove("opacity-0");
+        } else {
+            animationItem.style.opacity = 0;
+            animationItem.style.transform = "translateY(50px)";
+            animationItem.classList.add("opacity-0");
         }
-
-    })
-}
+    });
+};
 
 scrollAnimation();
 
-window.addEventListener("scroll",scrollAnimation)
+window.addEventListener("scroll", scrollAnimation);
